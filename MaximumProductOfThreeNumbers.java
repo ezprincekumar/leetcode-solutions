07a789e7dc5c2b5804ec2fc0de93cc4e1abbdbaa
Prince Kumar <213578506+ezprincekumar@users.noreply.github.com>
2026-07-27 17:11:17 +0530

    Add Solution
    Maximum Product of Three Numbers

---
+++ MaximumProductOfThreeNumbers.java
@@ -0,0 +1,33 @@
+public class MaximumProductOfThreeNumbers {
+    public int maximumProduct(int[] nums) {
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+
+        for (int n : nums) {
+            if (n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (n > max3) {
+                max3 = n;
+            }
+
+            if (n < min1) {
+                min2 = min1;
+                min1 = n;
+            } else if (n < min2) {
+                min2 = n;
+            }
+        }
+        return Math.max(max1 * max2 * max3, max1 * min1 * min2);
+    }
+
+    public static void main(String[] args) {
+        MaximumProductOfThreeNumbers solution = new MaximumProductOfThreeNumbers();
+        int[] nums = {-10, -10, 5, 2};
+        System.out.println("Maximum Product: " + solution.maximumProduct(nums)); //500
+    }
+}
