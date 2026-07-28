290f7d393f52e1363613ec6b42e90e126f5b9737
Prince Kumar <213578506+ezprincekumar@users.noreply.github.com>
2026-07-28 15:40:36 +0530

    Add "Palindrome Number" Solution

---
+++ PalindromeNumber.java
@@ -0,0 +1,22 @@
+public class PalindromeNumber {
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+
+    public static void main(String[] args) {
+        int[] testCases = {121, -121, 10, 0, 1221, 12321};
+
+        for (int num : testCases) {
+            System.out.println(num + " -> " + isPalindrome(num));
+        }
+    }
+}
