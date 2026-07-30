11c9948dc23b4d8985bed34f33290c824487d428
Prince Kumar <213578506+ezprincekumar@users.noreply.github.com>
2026-07-30 14:47:07 +0530

    Upload Solution
    Minimum Number of Pushes to Type Word I

---
+++ MinimumNumberOfPushesToTypeWordI.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class MinimumNumberOfPushesToTypeWordI {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+        
+        for (char c : word.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        Arrays.sort(freq);
+        
+        int pushes = 0;
+        int index = 0;
+        
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] == 0) break;
+            
+            int cost = (index / 8) + 1;
+            pushes += freq[i] * cost;
+            index++;
+        }
+        
+        return pushes;
+    }
+
+    public static void main(String[] args) {
+        MinimumNumberOfPushesToTypeWordI sol = new MinimumNumberOfPushesToTypeWordI();
+        System.out.println(sol.minimumPushes("abcde")); //5
+    }
+}
