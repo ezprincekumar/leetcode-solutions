4418d3030da15d61a611128303fac288357d9a16
Prince Kumar <213578506+ezprincekumar@users.noreply.github.com>
2026-08-02 23:39:45 +0530

    Upload "Stone Game" Solution File

---
+++ StoneGame.java
@@ -0,0 +1,11 @@
+public class StoneGame {
+  public boolean stoneGame(int[] piles) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        StoneGame sol = new StoneGame();
+        int[] piles = {3, 9, 1, 2};
+        System.out.println(sol.stoneGame(piles)); //true
+    }
+}
